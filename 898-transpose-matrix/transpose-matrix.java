7a1d1637065dd80aabcf846c71ffd6d286ea05b3
nabeelakhtar786 <137677788+nabeelakhtar786@users.noreply.github.com>
2025-07-31 20:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (37.37%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,15 +1,47 @@
-class Solution {
-    public static int[][] transpose(int[][] matrix) {
-        int m = matrix.length;       // number of rows
-        int n = matrix[0].length;    // number of columns
-        
-        int[][] transpose = new int[n][m];  // transpose will have dimensions n x m
-        
-        for (int i = 0; i < m; i++) {          // iterate over rows
-            for (int j = 0; j < n; j++) {      // iterate over columns
-                transpose[j][i] = matrix[i][j];
-            }
-        }
-        return transpose;
+class Solution{
+    public int[][] transpose(int[][] matrix)
+    { int i,j;
+int r = matrix.length;
+int c = matrix[0].length;
+int output[][]= new int[c][r]; 
+for(i =0; i < r; i++)
+    for(j=0; j < c; j++)
+        output[j][i]=matrix[i][j];
+
+    return output;
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public static int[][] transpose(int[][] matrix) {
+//         int m = matrix.length;       // number of rows
+//         int n = matrix[0].length;    // number of columns
+        
+//         int[][] transpose = new int[n][m];  // transpose will have dimensions n x m
+        
+//         for (int i = 0; i < m; i++) {          // iterate over rows
+//             for (int j = 0; j < n; j++) {      // iterate over columns
+//                 transpose[j][i] = matrix[i][j];
+//             }
+//         }
+//         return transpose;
+//     }
+// }
